454447d5e4e4afb20c227f7a10df736fc833bbf9
namle133 <leon.lethanhnam@gmail.com>
2022-05-17 15:31:01 +0700

    SQL_PRO

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+func main() {
+	conn, err := sql.Open("pgx", "host=localhost port=5432 dbname=SQL_PROJECT user=postgres password=Namle311")
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Unable to connect: %v", err))
+	}
+	defer conn.Close()
+	log.Println("Connected to database")
+}
